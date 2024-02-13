8d7dc8783b9a2a659ce40e96d336b3917cf28adc
sergeyv <sergeyv@genustechnologies.com>
2024-02-13 13:48:57 +0300

    changes

--- tests/_api/test_build.py
+++ tests/_api/test_build.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-@allure.feature("Build Info")
-@allure.story("Get Build Info")
-@allure.title("Get Build Info Test")
-def test_get_build_info(api_request_context: APIRequestContext):
+@allure.feature("Collections")
+@allure.story("Get Public Collections Hierarchy")
+@allure.title("Get Public Collections Hierarchy Test")
+def test_get_public_collections_hierarchy(api_request_context: APIRequestContext):
     response = api_request_context.get('/mediaupshot/services/contentEngine/json/hierarchy/public')
     print(response.headers)
     if response.status != 200:
@@ -18,5 +18,21 @@ def test_get_build_info(api_request_context: APIRequestContext):
     print(response.status)
     print(response_json_content)
     print(type(response_json_content['collection']))
-    for collection in response_json_content['collection']:
-        print(collection['name'])
+    dict = [collection['name'] for collection in response_json_content['collection']]
+    print(dict)
+    assert 'MU+Smoke+Test' in dict
+ 
+
+@allure.feature("Build Info")
+@allure.story("Get Build Info")
+@allure.title("Get Build Info Test")
+def test_get_build_info(api_request_context: APIRequestContext):
+    response = api_request_context.get('/mediaupshot/services/contentEngine/json/build-info')
+    print(response.headers)
+    if response.status != 200:
+        raise ValueError(f"Request test_get_build_info failed {response.status}")
+    response_json_content = response.json()
+    print(response.status)
+    print(response_json_content)
+    print(f"{response_json_content['product-name']} version:{response_json_content['product-version']}")
+    assert response_json_content['product-version'] == '7.1.0.6'
